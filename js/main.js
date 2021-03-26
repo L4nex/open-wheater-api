f424ffb97bcc247648d4fed440a8afcb5b80132e
L4nex <mchadolucas@gmail.com>
2021-03-25 21:41:53 -0300

    Programa finalizado(parcialmente)

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
-  
-$(function() {
-    const tbody = $('.table tbody');
 
-    function obterDados() {
-        $.ajax(`api.openweathermap.org/data/2.5/weather?q=blumenau&appid=86682086e6145a8827d33bd6e66a398f`, {
+$(function () {
+    const info = $('#info');
+    $('#submit').on('click', function(e) {
+        var city = $('#city').val();
+        obterDados(city)
+    })
+
+    function obterDados(city) {
+        $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=86682086e6145a8827d33bd6e66a398f&lang=pt_br&units=metric`, {
             async: true,
             crossDomain: true,
-            type : 'GET',
-            beforeSend: function() {
-                $('.table').after('<p class="loading">Aguarde Carregando ... </p>');
+            type: 'GET',
+            beforeSend: function () {
+                $('#info').after('<p class="loading">Aguarde Carregando ... </p>');
             },
-            error: function() {
-                $('.table').after('<p class="loading">Deu Ruim</p>');
+            error: function () {
+                $('#info').after('<p class="loading">Deu Ruim</p>');
             },
-            success: function(dados){
+            success: function (dados) {
                 mostrarDados(dados);
             },
-            complete: function() {
+            complete: function () {
                 $('.loading').remove();
             }
         })
 
         function mostrarDados(dados) {
-            $.each(dados, function(i, el) {
-                tbody.append(`<tr class="linha">
-                                <th scope="row" class="current_temp">${el.main.temp}</th>
-                                <td class="temp_max">${el.main.temp_max}</td>
-                                <td class="temp_min">${el.main.temp_min}</td>
-                                <td class="humidity">${el.main.humidity}%</td>                         
-                             </tr>
+            console.log(dados)
+            info.append(`<div class="card" style="width: 18rem; margin: 0 auto; margin-top: 1%;">
+                                <img src="images/ceu_limpo.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <div style="float: left;">
+                                        <p style="font-weight: bold; font-size: 18px;">${dados.name}</p>
+                                    </div>
+                                    <div style="float: right;">
+                                        <p style="font-weight: bold; font-size: 18px;">${dados.main.temp}°</p>
+                                    </div>
+                                    <br>
+                                    <br>
+                                    <p class="card-text">
+                                    ${dados.weather[0].description[0].toUpperCase()+ dados.weather[0].description.substr(1)} </p>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Temperatura máxima: ${dados.main.temp_max}°</li>
+                                    <li class="list-group-item">Temperatura mínima: ${dados.main.temp_min}°</li>
+                                    <li class="list-group-item">Humidade: ${dados.main.humidity}%</li>
+                                </ul>   
+                            </div>
              `)
-             console.log(el)
-            })
         }
     }
-    obterDados();
 });
